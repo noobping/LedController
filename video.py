84ba77ba49501023587e76c732690d54521a6721
Nick <124168669+nicksware@users.noreply.github.com>
2025-01-13 16:08:07 +0000

    Remove unused threading import from video.py

--- video.py
+++ video.py
@@ -8,7 +8,6 @@ import numpy as np  # For numerical operations
 import argparse
 import asyncio
 import keyboard  # Ensure you have installed 'keyboard' package
-import threading
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(levelname)s %(message)s")
