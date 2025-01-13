1eabed4c02404da362f7e045d708a7b5f645cd8d
Nick <124168669+nicksware@users.noreply.github.com>
2025-01-13 14:16:08 +0100

    Change logging level to INFO and reorder WLED IPs for clarity

--- piano.py
+++ piano.py
@@ -10,13 +10,13 @@ except ImportError:
     print("Please install the 'keyboard' package (pip install keyboard).")
     exit(1)
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(levelname)s %(message)s")
 
 # The IPs of your four WLED controllers
 WLED_IPS = [
-    "192.168.107.122",  # Index 0 -> Top Right
     "192.168.107.123",  # Index 1 -> Top Left
+    "192.168.107.122",  # Index 0 -> Top Right
     "192.168.107.120",  # Index 2 -> Bottom Right
     "192.168.107.121",  # Index 3 -> Bottom Left
 ]
