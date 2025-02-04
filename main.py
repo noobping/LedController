b778cd487ffe15f361e01c21d86cc0c74ec60da4
Nick <124168669+nicksware@users.noreply.github.com>
2025-02-04 11:25:22 +0100

    Change server port from 80 to 8901 for application startup

--- main.py
+++ main.py
@@ -804,4 +804,4 @@ async def broadcast_logs():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8901, reload=False)
